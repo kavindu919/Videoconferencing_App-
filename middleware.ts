621bf9104b67536ea228dc7e35cb2c4388110a25
kavindu919 <jayakodyk2@gmail.com>
2024-05-26 19:06:19 +0530

    make home page complete

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+//make  protected routes
+const protectedRoutes = createRouteMatcher([
+  "/",
+  "/upcoming",
+  "/meeting(.*)",
+  "/previous",
+  "/recordings",
+  "/personal-room",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
