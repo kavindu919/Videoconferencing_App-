da80a2944e42094559041756525608a9373d5328
kavindu919 <jayakodyk2@gmail.com>
2024-05-29 22:29:11 +0530

    complete the all aspects

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,6 +8,7 @@ import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useToast } from "@/components/ui/use-toast";
 import { Textarea } from "./ui/textarea";
 import ReactDatePicker from "react-datepicker";
+import { Input } from "./ui/input";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -161,6 +162,20 @@ const MeetingTypeList = () => {
         buttonText="Start Meeting"
         handleClick={createMeeting}
       />
+      <MeetingModal
+        isOpen={meetingState === "isJoiningMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder="Meeting Link"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+      </MeetingModal>
     </section>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -41,7 +41,6 @@ const Sidebar = () => {
           );
         })}
       </div>
-      Sidebar
     </div>
   );
 };
